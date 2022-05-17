f0cc112d8cc6dbec92497bc70ac55c97009e4ff0
ingeniousnet <105630703+ingeniousnet@users.noreply.github.com>
2022-05-17 17:27:37 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/article2',
+        destination: '/Aquamaof-breaks-ties-with-8F-and-Pure-Salmon',
+        permanent: true,
+      },
+    ]
+  },
+}
